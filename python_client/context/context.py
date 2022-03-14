bbeaa51bf295fd3a4f476d57d58e6052fb21b64e
Dariush <mohandesdariush@gmail.com>
2022-03-14 17:39:51 +0100

    inconsistent state handling
    functions for rule management

--- python_client/context/context.py
+++ python_client/context/context.py
@@ -2,6 +2,7 @@ from multiprocessing import Queue
 from transitions import Machine, State, EventData
 import Pyro4
 import itertools as it
+import time
 
 daemon = Pyro4.Proxy("PYRONAME:handlers")
 '''
@@ -35,19 +36,31 @@ def run(packet_queue: Queue, pub_list, sub_list, broker_list, relations):
 
 
 class DeviceState(State):
-    def __init__(self, name, state):
-        super().__init__(name)
+    def __init__(self, name, state, is_consistent):
+        super().__init__(name, on_enter="enter", on_exit="exit")
         self.state = state
+        self.is_consistent = is_consistent
+
+    def __repr__(self):
+        return str(self.state)
+
+    def enter(self, event):
+        if not self.is_consistent:
+            print("Entering inconsistent state !")
+
+    def exit(self, event):
+        if not self.is_consistent:
+            print("Leaving inconsistent state !")
 
 
 class NetworkContext(object):
-    def __init__(self, initial_state, state_combinations, abstract_rules, forbidden_states, state_inference):
+    def __init__(self, initial_state, state_combinations, abstract_rules, inconsistent_states, state_inference):
 
         self.abstract_rules = abstract_rules
         self.states = []
         self.transitions = []
-        self.transitions_change = {}
-        self.forbidden_states = forbidden_states
+        self.transitions_change = dict()
+        self.inconsistent_states = inconsistent_states
         self.state_inference = state_inference
 
         initial_state_name = ""
@@ -59,56 +72,65 @@ class NetworkContext(object):
                 initial_state_name = str(i)
             # print(state_src)
 
-            if self.is_allowed(state_src):
-                self.states.append(DeviceState(str(i), state_src))
+            is_src_consistent = self.is_consistent(state_src)
+            self.states.append(DeviceState(str(i), state_src, is_src_consistent))
 
-                j = 0
-                for state_dst in it.product(*(state_combinations[Name] for Name in state_combinations)):
-                    state_dst = dict(zip(state_combinations.keys(), state_dst))
-                    # print(state_dst)
+            j = 0
+            for state_dst in it.product(*(state_combinations[Name] for Name in state_combinations)):
+                state_dst = dict(zip(state_combinations.keys(), state_dst))
+                # print(state_dst)
 
-                    if self.is_allowed(state_dst):
+                is_dst_consistent = self.is_consistent(state_dst)
 
-                        # get all keys in state_src and state dst which doesn't have the same value
-                        diff_keys = [key for key in state_src.keys() & state_dst if state_src[key] != state_dst[key]]
+                # get all keys in state_src and state dst which doesn't have the same value
+                diff_keys = [key for key in state_src.keys() & state_dst if state_src[key] != state_dst[key]]
 
-                        conforming = False
+                conforming = False
 
-                        # no state self loop and transition only between states with 1 difference
-                        if len(diff_keys) == 1:
-                            infer = self.state_inference.get((diff_keys[0], state_dst[diff_keys[0]]))
-                            if infer is None or state_dst[infer[0]] == infer[1]:
-                                conforming = True
-                                # print(str(state_src[key[0]]) + " != " + str(state_dst[key[0]]))
-                                self.transitions_change[(str(i), str(j))] = {
-                                    diff_keys[0]: [state_src[diff_keys[0]], state_dst[diff_keys[0]]]}
+                # no state self loop and transition only between states with 1 difference
+                if len(diff_keys) == 1:
+                    infer = self.state_inference.get((diff_keys[0], state_dst[diff_keys[0]]))
+                    if infer is None or state_dst[infer[0]] == infer[1]:
+                        conforming = True
+                        # print(str(state_src[key[0]]) + " != " + str(state_dst[key[0]]))
+                        self.transitions_change[(str(i), str(j))] = {"change": {
+                            diff_keys[0]: [state_src[diff_keys[0]], state_dst[diff_keys[0]]]},
+                            "actions": []
+                        }
 
-                        # 2 different keys between state_src and state_dst, changing one key could infer a change
-                        # in the second one resulting in state_dst with 2 different fields without intermediate state
-                        elif len(diff_keys) == 2:
-                            infer1 = self.state_inference.get((diff_keys[0], state_dst[diff_keys[0]]))
-                            infer2 = self.state_inference.get((diff_keys[1], state_dst[diff_keys[1]]))
+                # 2 different keys between state_src and state_dst, changing one key could infer a change
+                # in the second one resulting in state_dst with 2 different fields without intermediate state
+                elif len(diff_keys) == 2:
+                    infer1 = self.state_inference.get((diff_keys[0], state_dst[diff_keys[0]]))
+                    infer2 = self.state_inference.get((diff_keys[1], state_dst[diff_keys[1]]))
 
-                            condition1 = infer1 and infer1 == (diff_keys[1], state_dst[diff_keys[1]])
-                            condition2 = infer2 and infer2 == (diff_keys[0], state_dst[diff_keys[0]])
-                            if condition1:
-                                conforming = True
-                                self.transitions_change[(str(i), str(j))] = {
-                                    diff_keys[0]: [state_src[diff_keys[0]], state_dst[diff_keys[0]]],
-                                    diff_keys[1]: [state_src[diff_keys[1]], state_dst[diff_keys[1]]]}
+                    condition1 = infer1 and infer1 == (diff_keys[1], state_dst[diff_keys[1]])
+                    condition2 = infer2 and infer2 == (diff_keys[0], state_dst[diff_keys[0]])
+                    if condition1:
+                        conforming = True
+                        self.transitions_change[(str(i), str(j))] = {"change": {
+                            diff_keys[0]: [state_src[diff_keys[0]], state_dst[diff_keys[0]]],
+                            diff_keys[1]: [state_src[diff_keys[1]], state_dst[diff_keys[1]]]},
+                            "actions": []
+                        }
 
-                            if condition2:
-                                conforming = True
-                                self.transitions_change[(str(i), str(j))] = {
-                                    diff_keys[1]: [state_src[diff_keys[1]], state_dst[diff_keys[1]]],
-                                    diff_keys[0]: [state_src[diff_keys[0]], state_dst[diff_keys[0]]]}
+                    if condition2:
+                        conforming = True
+                        self.transitions_change[(str(i), str(j))] = {"change": {
+                            diff_keys[1]: [state_src[diff_keys[1]], state_dst[diff_keys[1]]],
+                            diff_keys[0]: [state_src[diff_keys[0]], state_dst[diff_keys[0]]]},
+                            "actions": []
+                        }
 
-                        if conforming:
-                            self.transitions.append(
-                                {'trigger': 'evaluate', 'source': str(i), 'dest': str(j),
-                                 'conditions': 'check_transition',
-                                 'after': 'action'})
-                    j += 1
+                if conforming:
+                    transition = {'trigger': 'evaluate', 'source': str(i), 'dest': str(j),
+                                  'conditions': 'check_transition', 'after': 'action'}
+
+                    self.transitions.append(transition)
+                    if is_src_consistent and is_dst_consistent:
+                        self.set_transition_action(transition, state_src, state_dst)
+
+                j += 1
 
             i += 1
 
@@ -118,24 +140,15 @@ class NetworkContext(object):
         self.machine = Machine(model=self, states=self.states, transitions=self.transitions, initial=initial_state_name,
                                send_event=True)
 
-    def show_current_state(self):
-        print(self.machine.get_state(self.state).state)
-
-    def is_allowed(self, state):
-        for forbidden in self.forbidden_states:  # no forbidden state
+    def is_consistent(self, state):
+        for forbidden in self.inconsistent_states:  # no forbidden state
             if forbidden.items() <= state.items():
                 return False
 
         return True
 
-    def action(self, event):
-        # print("action: " + str(event))
-        data = event.kwargs["data"]
-
-        previous_state: DeviceState = self.machine.get_state(event.transition.source)
-        current_state: DeviceState = self.machine.get_state(event.transition.dest)
-
-        changed_element = self.transitions_change[(event.transition.source, event.transition.dest)]
+    def set_transition_action(self, transition, state_src, state_dst):
+        changed_element = self.transitions_change[(transition["source"], transition["dest"])]["change"]
         iterator = iter(changed_element)
         key1 = next(iterator, None)
         key2 = next(iterator, None)  # can be None
@@ -144,20 +157,96 @@ class NetworkContext(object):
             condition = rule["condition"]
 
             if key1 in condition or key2 in condition:
+                action = None
 
                 # if condition of rule does no longer hold
-                if condition.items() <= previous_state.state.items():
-                    print("reverse action : " + str(rule["action"]))
+                if condition.items() <= state_src.items():
+                    action = {"index": rule["index"], "reverse action": rule["action"]}
 
                 # if condition of rule now holds
-                if condition.items() <= current_state.state.items():
-                    print("action : " + str(rule["action"]))
+                if condition.items() <= state_dst.items():
+                    action = {"index": rule["index"], "action": rule["action"]}
+
+                if action:
+                    self.transitions_change[(transition["source"], transition["dest"])]["actions"].append(action)
+
+    def add_rule(self, rule):
+        index = 0
+        for abs_rule in self.abstract_rules:
+            if index < abs_rule["index"]:
+                break
+            else:
+                index += 1
+
+        rule["index"] = index
+        self.abstract_rules.append(rule)
+
+        for state_src in self.states:
+            for state_dst in self.states:
+                if state_src.name != state_dst.name:
+                    changed_element = self.transitions_change.get((state_src.name, state_dst.name))
+                    if changed_element:  # if transition between both states
+                        changed_element = changed_element["change"]
+                        iterator = iter(changed_element)
+                        key1 = next(iterator, None)
+                        key2 = next(iterator, None)  # can be None
+
+                        condition = rule["condition"]
+
+                        if key1 in condition or key2 in condition:
+                            action = None
+
+                            # if condition of rule does no longer hold
+                            if condition.items() <= state_src.state.items():
+                                action = {"index": rule["index"], "reverse action": rule["action"]}
+
+                            # if condition of rule now holds
+                            if condition.items() <= state_dst.state.items():
+                                action = {"index": rule["index"], "action": rule["action"]}
+
+                            if action:
+                                self.transitions_change[(state_src.name, state_dst.name)]["actions"].append(action)
+
+        return rule["index"]
+
+    def del_rule(self, rule_index):
+        deleted = False
+        for i in range(len(self.abstract_rules)):
+            if self.abstract_rules[i]["index"] == rule_index:
+                del self.abstract_rules[i]
+                deleted = True
+                break
+
+        if deleted:
+            for state_src in self.states:
+                for state_dst in self.states:
+                    if state_src != state_dst:
+                        changed_element = self.transitions_change.get((state_src.name, state_dst.name))
+                        if changed_element:  # if transition between both states
+                            for action in changed_element["actions"]:
+                                if action["index"] == rule_index:
+                                    changed_element["actions"].remove(action)
+                                    break
+            return rule_index
+        else:
+            return -1
+
+    def show_current_state(self):
+        print(self.machine.get_state(self.state).state)
+
+    def is_current_state_consistent(self):
+        return self.machine.get_state(self.state).is_consistent
+
+    def action(self, event):
+        # print("action: " + str(event))
+        actions = self.transitions_change[(event.transition.source, event.transition.dest)]["actions"]
+        print(actions)
 
     def check_transition(self, event):
         # print("condition: " + str(event.kwargs["data"]))
         data = event.kwargs["data"]
 
-        element = self.transitions_change[(event.transition.source, event.transition.dest)]
+        element = self.transitions_change[(event.transition.source, event.transition.dest)]["change"]
 
         element_key = next(iter(element))
 
@@ -185,29 +274,45 @@ def test():
                           "heater.status": ["on", "off"], "lamp.status": ["on", "off"]}
 
     # explicitly forbid state
-    forbidden_states = [{"heater.status": "on", "window.status": "open"}, {"thermo.temp": "hot", "heater.status": "on"}]
+    inconsistent_states = [{"heater.status": "on", "window.status": "open"},
+                           {"thermo.temp": "hot", "heater.status": "on"}]
 
     # infer state of one or multiple devices when a message from another device is received
     # TODO : make it possible to infer the state of multiples devices from one message
     #  (array of tuples instead of one tuple in the value), len(diff_keys) will be 2 or greater now
-    state_inference = {("thermo.temp", "hot"): ("heater.status", "off"), # for example add ("air_conditioner.status": "on")
+    state_inference = {("thermo.temp", "hot"): ("heater.status", "off"),
+                       # for example add ("air_conditioner.status": "on")
                        ("mvt_sensor.lastMessage", "recent"): ("lamp.status", "on")}
 
     # contains actions to take if condition is met
-    abstract_rules = [
-        {"condition": {"thermo.temp": "hot", "heater.status": "off"},
-         "action": {"disable": {"src": "heater_switch", "dst": "heater"}}},
-        {"condition": {"mvt_sensor.lastMessage": "long", "window.status": "closed"},
-         "action": {"disable": {"src": "window_switch", "dst": "window"}}}]
+    abstract_rules = [{"index": 0, "condition": {"mvt_sensor.lastMessage": "long", "window.status": "closed"},
+                       "action": {"disable": {"src": "window_switch", "dst": "window"}}}
+                      ]
 
     # last message triggers a timer in the machine fms after the .trigger call ?
-    network_context = NetworkContext(initial_state, state_combinations, abstract_rules, forbidden_states,
+    network_context = NetworkContext(initial_state, state_combinations, abstract_rules, inconsistent_states,
                                      state_inference)
+
+    rule = {"condition": {"thermo.temp": "hot", "heater.status": "off"},
+            "action": {"disable": {"src": "heater_switch", "dst": "heater"}}}
+
+    rule_index = network_context.add_rule(rule)
+    # rule_index = network_context.del_rule(rule_index)
+    # rule_index = network_context.add_rule(rule)
+
     network_context.show_current_state()
     network_context.evaluate(data=("thermo.temp", "cold"))
     network_context.show_current_state()
     network_context.evaluate(data=("thermo.temp", "hot"))
     network_context.show_current_state()
+    network_context.evaluate(data=("heater.status", "on"))
+    network_context.show_current_state()
+
+    ''' For efficiency test later
+    start = time.time()
+    end = time.time()
+    print(end - start)
+    '''
 
 
 def get_element(json, field):
